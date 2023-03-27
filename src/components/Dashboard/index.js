3e19aa79de35ea60595b8aab2a23a7f0e11fe26c
ulfathAkf <114693849+ulfathAkf@users.noreply.github.com>
2023-03-27 12:35:48 +0530

    update the fe files

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../Navbar/Nav";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
     <>
-    <div>Start a new Document</div>
+      <Navbar />
+      <div className="container-main">
+        <div className="header">New Application</div>
+        <div className="page-container">
+        <div className="template">
+          <div className="content">
+            <div className="content-card">
+            <i class="bi bi-plus-circle"><Link to="/login"> + </Link></i>
+                
+            </div>
+            <div className="content-card">templ</div>
+            <div className="content-card">template 1</div>
+            <div className="content-card">template 1</div>
+            <div className="content-card">template 1</div>
+          </div>
+        </div>
+        <div className="recent">
+        <div className="header">My Projects</div>
+
+        <div className="content">
+            <div className="content-card"></div>
+            <div className="content-card">template 1</div>
+            <div className="content-card">template 1</div>
+            <div className="content-card">template 1</div>
+            <div className="content-card">template 1</div>
+          </div>
+        </div>
+        </div>
+      </div>
     </>
   );
 };
