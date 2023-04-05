4571c54fb101edf8e8afd1a70611618896b16c86
Sara <zeinabsarais27@gmail.com>
2023-04-05 12:42:12 +0530

    Changes for Workflow

--- src/Workflow/Components/NewComponents/GeneralConfiguration/Pop.js
+++ src/Workflow/Components/NewComponents/GeneralConfiguration/Pop.js
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
-import { FaUserCircle, FaTrash } from "react-icons/fa";
-import { GrAppsRounded } from "react-icons/gr";
+import { FaUserCircle } from "react-icons/fa";
+import { GrAppsRounded, GrConfigure } from "react-icons/gr";
 import { MdOutlineSettingsSuggest } from "react-icons/md";
-// import Configure from "./Workflow/Components/NewComponents/AddEvent/GeneralConfiguration/Configure";
-// import Configure from "./Configure";
-// import Configure from "./Configure";
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import "./Configure.css";
+import Configure from "./GeneralConfiguration/Configure";
 import "./Pop.css";
 
 export default function Pop() {
@@ -26,21 +22,17 @@ export default function Pop() {
     <>
       <div className="token-flex">
         <div className="token-box" onClick={toggleModal}>
-          click to add event...
+          click to add event..
         </div>{" "}
         &nbsp;&nbsp;&nbsp;
-        <div className="token" onClick={toggleModal}>
-          click to add event..
-          <FaTrash className="trash" />
-          {modal && (
-            <div className="modal">
-              <div onClick={toggleModal} className="overlay"></div>
-              <div className="modal-content"></div>
-            </div>
-          )}
-        </div>
+        {modal && (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content"></div>
+          </div>
+        )}
       </div>
-
+      {modal && <Configure></Configure>}
       {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
@@ -49,50 +41,42 @@ export default function Pop() {
               <div className="dropdown">
                 <div className="ele1">
                   <FaUserCircle /> &nbsp;&nbsp;&nbsp; General
-                  <div className="dropdown-content">
-                    <a href="1">User is logged in</a>
-
-                    {/* <a href="2" onClick={() => <Configure></Configure>}>User is logged out</a> */}
-                    <a href="3">Page is loaded</a>
-                    <a href="4">Do every 5 seconds</a>
-                    <a href="5">Do when condition is True</a>
-                    <a href="6">An unhandled error occurs</a>
-                  </div>
+                  <ul className="dropdown-content">
+                    <button onClick={Configure}>User logged in</button>
+                    <li>Page is loaded</li>
+                    <li>Do every 5 seconds</li>
+                    <li>Do when condition is True</li>
+                    <li>An unhandled error occurs</li>
+                  </ul>
                 </div>
               </div>
               <br></br>
-              {/* <br></br> */}
+
               <hr></hr>
 
               <div className="dropdown">
                 <div className="ele2">
                   <GrAppsRounded /> &nbsp;&nbsp;&nbsp; Elements
-                  <div className="dropdown-content">
-                    <a href="1">An element is clicked</a>
-                    <a href="2">An input's value is changed</a>
-                    <a href="3">A map's marker is clicked</a>
-                    <a href="4">A popup is closed</a>
-                    <a href="5">A popup is opened</a>
-                    <a href="6">An element has an error running a workflow</a>
-
-                    {/* <datalist>
-                <option value="Interner">Internet</option>
-                <option value="Interner">Internet</option>
-                <option value="Interner">Internet</option>
-              </datalist> */}
-                  </div>
+                  <ul className="dropdown-content" onClick={toggleModal}>
+                    <li>An element is clicked</li>
+                    <li>An input's value is changed</li>
+                    <li>A map's marker is clicked</li>
+                    <li>A popup is closed</li>
+                    <li>A popup is opened</li>
+                    <li>An element has an error running a workflow</li>
+                  </ul>
                 </div>
               </div>
               <br></br>
-              {/* <br></br> */}
+
               <hr></hr>
 
               <div className="dropdown">
                 <div className="ele3">
                   <MdOutlineSettingsSuggest /> &nbsp;&nbsp;&nbsp; Custom
-                  <div className="dropdown-content">
-                    <a href="1">Create a custom event..</a>
-                  </div>
+                  <ul className="dropdown-content">
+                    <li>Create a custom event..</li>
+                  </ul>
                   <br></br>
                 </div>
               </div>
