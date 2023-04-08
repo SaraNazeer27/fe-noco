95ec3de4c150b598c41badba6149ba40705f0fbf
Sara <zeinabsarais27@gmail.com>
2023-04-08 22:08:55 +0530

    Removed the commented Changes

--- src/Workflow/Components/EventList/EventList.js
+++ src/Workflow/Components/EventList/EventList.js
@@ -15,50 +15,6 @@
 // };
 
 // export default EventList;
-// import React from "react";
-// import "./EventList.css";
-
-// const EventList = (props) => {
-//   const handleEventClick = (event) => {
-//     props.onEventClick(event);
-//   };
-
-//   return (
-//     <div className="token-flex-container">
-//       {props.addedEvents &&
-//         props.addedEvents.map((addedEvent) => (
-//           <div
-//             className="token-flex"
-//             key={addedEvent.id}
-//             onClick={() => handleEventClick(addedEvent)}
-//           >
-//             <div className="token-boxSaved">{addedEvent.name.toString()}</div>
-//           </div>
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default EventList;
-
-// import React from "react";
-// import "./EventList.css";
-
-// const EventList = (props) => {
-//   return (
-//     <div className="token-flex-container">
-//       {props.addedEvents &&
-//         props.addedEvents.map((addedEvent) => (
-//           <div className="token-flex" key={addedEvent.id}>
-//             <div className="token-box">{addedEvent.name.toString()}</div>
-//             &nbsp;&nbsp;&nbsp;
-//           </div>
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default EventList;
 
 import React from "react";
 import "./EventList.css";
