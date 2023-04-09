404feda1288fc419aa9b7237fdf09644eefd94f5
Sara <zeinabsarais27@gmail.com>
2023-04-09 10:37:52 +0530

    Changes added to EventList component

--- src/Workflow/Components/EventList/EventList.js
+++ src/Workflow/Components/EventList/EventList.js
@@ -17,6 +17,37 @@
 // export default EventList;
 
 // Correct Code
+// import React from "react";
+// import { FaRegTrashAlt } from "react-icons/fa";
+// import "./EventList.css";
+
+// const EventList = (props) => {
+//   const handleEventClick = (addedEvent) => {
+//     props.onEventClick(addedEvent);
+//   };
+
+//   return (
+//     <div className="token-flex-container">
+//       {props.addedEvents &&
+//         props.addedEvents.map((addedEvent) => (
+//           <div
+//             className="token-flex"
+//             key={addedEvent.id}
+//             onClick={() => handleEventClick(addedEvent)}
+//           >
+//             <div className="token-boxSaved position-relative">
+//               <h3>When</h3>
+//               {addedEvent.name.toString()}
+//               <FaRegTrashAlt className="faReg" />
+//             </div>
+//           </div>
+//         ))}
+//     </div>
+//   );
+// };
+
+// export default EventList;
+
 import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 import "./EventList.css";
@@ -24,6 +55,13 @@ import "./EventList.css";
 const EventList = (props) => {
   const handleEventClick = (addedEvent) => {
     props.onEventClick(addedEvent);
+  };
+
+  const handleDeleteClick = (addedEvent) => {
+    const updatedEvents = props.addedEvents.filter(
+      (event) => event.id !== addedEvent.id
+    );
+    props.onDeleteClick(updatedEvents);
   };
 
   return (
@@ -38,7 +76,10 @@ const EventList = (props) => {
             <div className="token-boxSaved position-relative">
               <h3>When</h3>
               {addedEvent.name.toString()}
-              <FaRegTrashAlt className="faReg" />
+              <FaRegTrashAlt
+                className="faReg"
+                onClick={() => handleDeleteClick(addedEvent)}
+              />
             </div>
           </div>
         ))}
