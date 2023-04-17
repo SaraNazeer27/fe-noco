4a059bdc6d02764be259f7c0fff7d6365062df59
SaraNazeer27 <zeinabsarais27@gmail.com>
2023-04-17 12:43:29 +0530

    breaking commented

--- src/ApiBinding/Components/ApiHome/ApiHome.js
+++ src/ApiBinding/Components/ApiHome/ApiHome.js
@@ -2,33 +2,28 @@ import React, { useState } from "react";
 import RestApi from "../RestApi/RestApi";
 import "./ApiHome.css";
 
-
 const API_TYPES = [
   {
- 
-            key: 1,
-            fieldType: "dropdown", //input number text
-            options: [
-              {
-                displayValue: "Rest",
-                value: "Rest",
-              },
-            ],
-          },
-          {
-            key: 2,
-            fieldType: "dropdown", //input number text
-            options: [
-              {
-                displayValue: "Soap",
-                value: "Soap",
-              },
-            ],
-          }
-        ],
-      
-
-
+    key: 1,
+    fieldType: "dropdown", //input number text
+    options: [
+      {
+        displayValue: "Rest",
+        value: "Rest",
+      },
+    ],
+  },
+  {
+    key: 2,
+    fieldType: "dropdown", //input number text
+    options: [
+      {
+        displayValue: "Soap",
+        value: "Soap",
+      },
+    ],
+  },
+];
 
 const ApiHome = () => {
   const [showModal, setShowModal] = useState(false);
@@ -41,25 +36,26 @@ const ApiHome = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+  return <div>ff</div>;
 
-  return (
-    <>
-    <div>
-      <button onClick={handleOpenModal}>+ New integration</button>
-      <div className="modal-content" onClick={closeModalContent}>
-            <ul>
-              {eventTypes.map((eventType) => (
-                <div className="dropdown" key={eventType.id}>
-                  </div>
-              ))}
-                  </ul>
+  // return (
+  //   <>
+  //   <div>
+  //     <button onClick={handleOpenModal}>+ New integration</button>
+  //     <div className="modal-content" onClick={closeModalContent}>
+  //           <ul>
+  //             {eventTypes.map((eventType) => (
+  //               <div className="dropdown" key={eventType.id}>
+  //                 </div>
+  //             ))}
+  //                 </ul>
 
-      {showModal && <RestApi onClose={handleCloseModal} /> && (
-        <RestApi onClose={handleCloseModal} />
-      )}
-      </div>
-    <>
-  );
+  //     {showModal && <RestApi onClose={handleCloseModal} /> && (
+  //       <RestApi onClose={handleCloseModal} />
+  //     )}
+  //     </div>
+  //   <>
+  // );
 };
 
 export default ApiHome;
