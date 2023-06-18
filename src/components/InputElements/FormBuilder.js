aafc5c00ca2cbc11a693b2be1e9a15fbf3f07dca
ulfathAkf <114693849+ulfathAkf@users.noreply.github.com>
2023-06-19 02:10:58 +0530

    changes

--- src/components/InputElements/FormBuilder.js
+++ src/components/InputElements/FormBuilder.js
@@ -8,7 +8,6 @@ import "./FileUpload.css";
 import Navbar from "../NavBar/navbar";
 
 const InputElements = () => {
-  const [selectedComponent, setSelectedComponent] = useState(null);
   const [components, setComponents] = useState([]);
   const [canvasComponents, setCanvasComponents] = useState([]);
 
@@ -31,7 +30,6 @@ const InputElements = () => {
     setCanvasComponents([...canvasComponents, componentWithPosition]);
   };
 
-
   const getComponentByType = (type) => {
     switch (type) {
       case "textEditor":
@@ -51,56 +49,6 @@ const InputElements = () => {
     event.dataTransfer.setData("componentType", componentType);
   };
 
-  const handleDragOver = (event) => {
-    event.preventDefault();
-  };
-
-  const handleDropInsideCanvas = (event) => {
-    event.preventDefault();
-    const droppedIndex = event.dataTransfer.getData("text/plain");
-    const droppedComponent = canvasComponents[droppedIndex];
-    const { clientX, clientY } = event;
-    const updatedComponents = canvasComponents.filter(
-      (_, index) => index !== parseInt(droppedIndex)
-    );
-    const componentWithUpdatedPosition = {
-      ...droppedComponent,
-      position: { x: clientX, y: clientY },
-    };
-    setCanvasComponents([...updatedComponents, componentWithUpdatedPosition]);
-  };
-
-  const handleComponentDragStart = (event, index) => {
-    event.dataTransfer.setData("text/plain", index);
-  };
-  const handleComponentMouseDown = (event, index) => {
-    event.preventDefault();
-    setSelectedComponent(index);
-  };
-
-  const handleComponentMouseMove = (event) => {
-    event.preventDefault();
-    if (selectedComponent !== null) {
-      const { clientX, clientY } = event;
-      const updatedComponents = canvasComponents.map((component, index) => {
-        if (index === selectedComponent) {
-          return {
-            ...component,
-            position: { x: clientX, y: clientY },
-          };
-        }
-        return component;
-      });
-      setCanvasComponents(updatedComponents);
-    }
-  };
-
-  const handleComponentMouseUp = () => {
-    setSelectedComponent(null);
-  };
-
-
-
   return (
     <>
       <Navbar />
@@ -113,7 +61,6 @@ const InputElements = () => {
             className="draggable-item"
             draggable="true"
             onDragStart={(event) => handleDragStart(event, "textEditor")}
-            
           >
             Text Editor
           </div>
@@ -142,33 +89,22 @@ const InputElements = () => {
         <div className="main-content">
           <div style={{ display: "flex" }}>
             <div
-               className="drop-container"
-               onDragOver={handleDragOver}
-               onDrop={handleDropCanvas}
+              className="drop-container"
+              onDragOver={(event) => event.preventDefault()}
+              onDrop={handleDropCanvas}
             >
               <h5 className="heading">Canvas Area</h5>
               {canvasComponents.map((component, index) => (
                 <div
                   key={index}
                   className="component"
-                 
-                  style={{
-                    position: "absolute",
-                    top: component.position.y,
-                    left: component.position.x,
-                  }}
+                  style={{ position: "absolute", top: component.position.y, left: component.position.x }}
                   draggable="true"
-                  onDragStart={(event) =>
-                    handleComponentDragStart(event, index)
-                  }
-                  onDragEnd={handleDropInsideCanvas}
-                
                 >
                   {component.component}
                 </div>
               ))}
             </div>
-            
           </div>
         </div>
       </div>
@@ -177,4 +113,3 @@ const InputElements = () => {
 };
 
 export default InputElements;
-
